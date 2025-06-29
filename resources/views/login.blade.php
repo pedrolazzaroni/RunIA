f17bdfcc00b0469557137df5122f3a98cbb48734
pedrolazzaroni <phlazzaroni@gmail.com>
2025-06-29 17:28:30 -0300

    Add space

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,14 +14,17 @@
             Faça login:
             <label for="email">Email:</label>
             <input class="outline-red-300 bg-amber-300" type="email" name="email" id="email">
+            
             <label for="password">Senha</label>
             <input class="outline-red-300 bg-amber-300" type="password" name="password" id="password">
+
             <button class="rounded-lg bg-amber-700 p-4 w-20 items-center" type="submit">Entrar</button>
         </form>
         <div class="bg-red-500 h-2">
 
         </div>
         <form action="{{route('register')}}" method="POST" class="p-4 bg-amber-50 w-auto rounded-lg flex flex-col gap-4 items-center justify-center">
+
             @csrf
             Faça Registro:
             <label for="email">Email:</label>
@@ -32,7 +35,7 @@
 
             <label for="password">Senha</label>
             <input class="outline-red-300 bg-amber-300" type="password" name="password" id="password">
-            
+
             <label for="password_confirmation">Confirmação de Senha</label>
             <input class="outline-red-300 bg-amber-300" type="password" name="password_confirmation" id="password_confirmation">
 
